d538f8d9cdf450c2b0bc57af495e81edff17e81c
Spyridon Peppas <spyridonpeppas@192.168.0.124>
2020-11-16 13:13:33 +0200

    inner join tables correction

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,11 @@ const router = express.Router();
 const conn = require('../server');
 
 router.get('/', (req,res) => {
-var sql = "SELECT  * FROM tbl_slideshow_home INNER JOIN tbl_product ON tbl_slideshow_home.product_id=tbl_product.product_id;";
-  var sql2 = "SELECT  * FROM tbl_slideshow_home INNER JOIN tbl_product_img ON tbl_slideshow_home.img_id=tbl_product_img.img_id;"
+var sql = "SELECT  * FROM tbl_slideshow_home INNER JOIN tbl_product_img ON tbl_slideshow_home.img_id=tbl_product_img.img_id INNER JOIN tbl_product ON tbl_slideshow_home.product_id=tbl_product.product_id;"
   let query = conn.query(sql, (err, results)=>{
     if(err) throw err;
-    let query2 = conn.query(sql2, (err, results2)=>{
-      if(err) throw err;
-      res.render('index',{ res_product_img: results2, res_product: results});
-    });
+    res.render('index',{ res_product_img: results });
   });
-   
-})
+});
 
 module.exports = router;
